a8a8621f3235f27e0432d901d04b915b7e6d291f
Mike Peiman <mikepeiman@gmail.com>
2018-07-10 15:52:32 -0700

    Built out basic CRUD functionality, validation, alerts, using state and ES2015 modules

--- src/app.js
+++ src/app.js
@@ -1,13 +1,112 @@
-import {http} from './http'
-import {ui } from './ui'
+import { http } from './http'
+import { ui } from './ui'
 
 document.addEventListener('DOMContentLoaded', getPosts)
+// listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost)
+// listen for delete post
+document.querySelector('#posts').addEventListener('click', deletePost)
+// listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit)
+// listen for cancel click
+document.querySelector('.card-form').addEventListener('click', cancelEdit)
 
+
+// get posts from DB
 function getPosts() {
   http.get('http://localhost:3000/posts')
-  .then((result) => {
-    ui.showPosts(result)
-  }).catch((err) => {
-    console.log(err)
-  });
+    .then((result) => {
+      ui.showPosts(result)
+    }).catch((err) => {
+      console.log(err)
+    });
+}
+
+// add post, or submit edited post via form
+function submitPost() {
+  const title = document.querySelector('#title').value
+  const body = document.querySelector('#message-body').value
+  const id = document.querySelector('#hidden-id').value
+
+  const data = {
+    title,
+    body
+  }
+  // validate input
+  if (title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger')
+  } else {
+    // check for edit state  by hidden ID field - only contains a value if in edit state
+    if (id === '') {
+      // we are in add state
+      // create post
+      http.post('http://localhost:3000/posts', data)
+        .then(data => {
+          ui.showAlert('Post added', 'alert alert-success')
+          ui.clearFields()
+          getPosts()
+        })
+        .catch(err => console.log(err))
+    } else {
+      // we are in edit state, update post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post updated', 'alert alert-success')
+        ui.changeFormState('add')
+        getPosts()
+      })
+      .catch(err => console.log(err))
+    }
+
+
+
+
+  }
+
+}
+
+// delete post
+function deletePost(e) {
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id
+    if (confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post removed', 'alert alert-success')
+          getPosts()
+        })
+        .catch(err => console.log(err))
+    }
+  }
+  e.preventDefault();
+
+}
+
+// enable edit state
+function enableEdit(e) {
+  console.log(e.target)
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent
+    const body = e.target.parentElement.previousElementSibling.textContent
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // fill form with current post
+    ui.fillForm(data)
+
+  }
+  e.preventDefault()
+}
+
+// cancel edit state
+function cancelEdit(e) {
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add')
+  }
+  e.preventDefault()
 }
--- src/ui.js
+++ src/ui.js
@@ -2,8 +2,8 @@ class UI {
   constructor() {
     this.post = document.querySelector('#posts')
     this.titleInput = document.querySelector('#title')
-    this.messageInput = document.querySelector('#message-body')
-    this.idInput = document.querySelector('#id')
+    this.bodyInput = document.querySelector('#message-body')
+    this.idInput = document.querySelector('#hidden-id')
     this.postSubmit = document.querySelector('.post-submit')
     this.forState = 'add'
   }
@@ -17,12 +17,98 @@ class UI {
         <div class="card-body">
           <h4 class="card-title">${post.title}</h4>
           <p class="card-text">${post.body}</p>
+          <a href="#" class="edit card-link" data-id="${post.id}">
+            <i class="fa fa-pencil"></i>
+          </a>
+          <a href="#" class="delete card-link" data-id="${post.id}">
+            <i class="fa fa-remove"></i>
+          </a>
         </div>
       </div>
       `
     })
 
     this.post.innerHTML = output
+  }
+
+  showAlert(message, className) {
+    this.clearAlert()
+
+    // create div
+    const div = document.createElement('div')
+    div.className = className
+    // add text
+    div.appendChild(document.createTextNode(message))
+    // get parent, in order to insert into DOM
+    const container = document.querySelector('.postsContainer')
+    // get posts div
+    const posts = document.querySelector('#posts')
+    // insert alert div
+    container.insertBefore(div, posts)
+
+    // set timeout
+    setTimeout(() => {
+      this.clearAlert()
+    }, 3000);
+  }
+
+  // remove alert after timeout
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert')
+    if(currentAlert) {
+      currentAlert.remove()
+    }
+  }
+
+  // clear input fields after submission
+  clearFields() {
+    this.titleInput.value = ''
+    this.bodyInput.value = ''
+  }
+
+  // fill form with existing post content to edit
+  fillForm(data) {
+    this.idInput.value = data.id
+    this.titleInput.value = data.title
+    this.bodyInput.value = data.body
+    
+    this.changeFormState('edit')
+  }
+
+  // clear id hidden value
+  clearIdInput() {
+    this.idInput.value = ''
+  }
+
+  // change the form state
+  changeFormState(state) {
+    if(state === 'edit') {
+      this.postSubmit.textContent = 'Update Post'
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block'
+
+      // create cancel button
+      const button = document.createElement('button')
+      button.className = 'post-cancel btn btn-block btn-dark'
+      button.appendChild(document.createTextNode('Cancel Edit'))
+      // get parent
+      const parent = document.querySelector('.card-form')
+      // get element to insert before - the 'span-end' span
+      const formEnd = document.querySelector('.form-end')
+      // insert new button
+      parent.insertBefore(button, formEnd)
+
+    } else {
+      this.postSubmit.textContent = 'Post It'
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block'
+      // remove cancel button if present
+      if(document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove()
+      }
+      // clear ID from hidden field
+      this.clearIdInput()
+      // clear content from input fields
+      this.clearFields()
+    }
   }
 }
 
